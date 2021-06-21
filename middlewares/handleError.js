e31451f6dfd01cc1eaaa8a5be445042ded0df0f1
I Gusti Bagus Vayupranaditya Putraadinatha <IGustiBagus.VayupranadityaPutraadinatha@mitrais.com>
2021-06-21 14:19:47 +0700

    handle default internal error

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,11 +1,20 @@
 "use strict";
 
 const handleError = (customErr, req, res, next) => {
-    if (customErr.logMsg) console.error(customErr.logMsg);
-    res.writeHead(customErr.status);
-    res.end(JSON.stringify({
-        err: customErr.responseMsg
-    }));
+    try {
+        if (customErr.logMsg) console.error(customErr.logMsg);
+        res.writeHead(customErr.status);
+        res.end(JSON.stringify({
+            err: customErr.responseMsg
+        }));
+    } catch (err) {
+        console.error(customErr);
+        console.error(err);
+        res.writeHead(500);
+        res.end(JSON.stringify({
+            err: 'internal_err'
+        }));
+    }
 }
 
 module.exports = handleError;
